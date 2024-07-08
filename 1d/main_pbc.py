07d0131a140c3462d1f4597239441fdb560cf749
Qile <yanqile1993@outlook.com>
2024-07-08 15:50:08 -0500

    update

--- 1d/main_pbc.py
+++ 1d/main_pbc.py
@@ -24,9 +24,9 @@ width=x1-x0
 nreq=1000
 target=0
 plotefuns=0,10,20,30,40,50,60,70,80,90,100,150,200,250,300,350,400,450,500,550,600,700,800,900,999
-plotefuns_2=[int(d) for d in range(120,160)]
+plotefuns_2=[int(d) for d in range(20)]
 bctype='periodic'
-coeftype='random displacement' #'fixed displacement'  # 'pw_2constant' #'constant' #'1d random displacement'
+coeftype='random displacement' #'fixed displacement' #'random displacement'  # 'pw_2constant' #'constant' 
 np.random.seed(10)
 params=''
 # create directory and filenames for output
