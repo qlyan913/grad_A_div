08fc001741ffdb4e94b13397730d628a03e07449
Qile <yanqile1993@outlook.com>
2024-08-10 17:59:22 -0500

    update

--- 2d/main.py
+++ 2d/main.py
@@ -17,8 +17,8 @@ import numpy as np
 from solver import *
 deg = 5
 L=1 # length of square
-nx=1000
-ny=1000
+nx=100
+ny=100
 nreq=1000
 target=0
 plotefuns=0,10,20,30,40,50,60,70,80,90,100,150,200,250,300,350,400,450,500,550,600,700,800,900,999
