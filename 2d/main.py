e739cd89e5776ce5417d292c70d0298aef0352fb
Qile <yanqile1993@outlook.com>
2024-08-12 19:14:15 -0500

    update

--- 2d/main.py
+++ 2d/main.py
@@ -20,7 +20,7 @@ L=20 # length of square
 nx=100
 ny=100
 nreq=301
-target=14
+target=20
 plotefuns=0,10,20,30,40,50,60,70,80,90,100,150,200,250,300
 plotefuns_2=[int(d) for d in range(20)]
 flag=1 # 1: print all first n_all(default=500) eigenfuns, 0: print plotefuns 
