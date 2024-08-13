85703da62ea52108d3f2cb177edccbadc7deb4d0
Qile <yanqile1993@outlook.com>
2024-08-13 00:59:47 -0500

    update

--- 1d/main_v2.py
+++ 1d/main_v2.py
@@ -24,8 +24,9 @@ nreq=1000
 target=0
 plotefuns=0,10,20,30,40,50,60,70,80,90,100,150,200,250,300,350,400,450,500,550,600,700,800,900,999
 plotefuns_2=[int(d) for d in range(20)]
+fv=2 # fv=1: f, fv=2, f version 2
 flag=1 # print all first 500 eigenfuns
-flag2 =3
+flag2 =1
 """
      flag2 ---- 1: -div A grad phi = lambda phi
           ---- 2: -div A grad phi = lambda A phi
@@ -108,13 +109,18 @@ else:
    f_sum=0.0
    for i in range(nn):
       x_center=i+1+dn[i]
+   if fv==1:
       f_sum=f_sum + conditional(abs(x-x_center)>s,0,6/8*(x-x_center)*(1-pow(x-x_center,2)/pow(s,2))**3-6/(8*pow(s,2))*(x-x_center)*(1-pow(x-x_center,2)/pow(s,2))**2*(3*pow(x-x_center,2)+1))
-   # near x=x0
-   x_center=x0+dn[nn]
-   f_sum=f_sum + conditional(abs(x-x_center)>s,0,6/8*(x-x_center)*(1-pow(x-x_center,2)/pow(s,2))**3-6/(8*pow(s,2))*(x-x_center)*(1-pow(x-x_center,2)/pow(s,2))**2*(3*pow(x-x_center,2)+1))
-   # near x=x1
-   x_center=x_center+x1-x0
-   f_sum=f_sum + conditional(abs(x-x_center)>s,0,6/8*(x-x_center)*(1-pow(x-x_center,2)/pow(s,2))**3-6/(8*pow(s,2))*(x-x_center)*(1-pow(x-x_center,2)/pow(s,2))**2*(3*pow(x-x_center,2)+1))
+   else:   
+      f_sum=f_sum + conditional(abs(x-x_center)>s,0,20*(1-((x-x_center)**2)/pow(s,2))**3*(3*((x-x_center)**2+1))
+   
+   if fv==1:
+      # near x=x0
+      x_center=x0+dn[nn]
+      f_sum=f_sum + conditional(abs(x-x_center)>s,0,6/8*(x-x_center)*(1-pow(x-x_center,2)/pow(s,2))**3-6/(8*pow(s,2))*(x-x_center)*(1-pow(x-x_center,2)/pow(s,2))**2*(3*pow(x-x_center,2)+1))
+      # near x=x1
+      x_center=x_center+x1-x0
+      f_sum=f_sum + conditional(abs(x-x_center)>s,0,6/8*(x-x_center)*(1-pow(x-x_center,2)/pow(s,2))**3-6/(8*pow(s,2))*(x-x_center)*(1-pow(x-x_center,2)/pow(s,2))**2*(3*pow(x-x_center,2)+1))
    aexpr=1./(1+f_sum)
    aelt='CG'
    adeg=7
