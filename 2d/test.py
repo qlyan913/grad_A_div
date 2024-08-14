d67eb4b8c243818d0703fc3757bfac68c8a81e8f
qlyan913 <yanqile1993@outlook.com>
2024-08-14 11:09:49 -0700

    update

---
+++ 2d/test.py
@@ -0,0 +1,40 @@
+import os,math
+import matplotlib.pyplot as plt
+from firedrake import *
+from firedrake.petsc import PETSc
+from firedrake.__future__ import interpolate
+from slepc4py import SLEPc
+import numpy as np
+from solver import *
+deg = 5
+L=100 # length of square
+nx=100
+ny=100
+mesh = SquareMesh(nx,ny,L)
+x , y= SpatialCoordinate(mesh)
+s=0.25
+nn=L-1
+# unifrom distribution on unit disk
+U1 = np.random.uniform(size = nn**2)
+U2 = np.random.uniform(size = nn**2)
+r = 0.2
+dn1 = r * np.sqrt(U2) * np.cos(2 * np.pi * U1)
+dn2 = r * np.sqrt(U2) * np.sin(2 * np.pi * U1)
+f_sum=0.0
+for i in range(0,nn):
+   for j in range(0,nn):
+      x_center=[i+1+dn1[i*nn+j],j+1+dn2[i*nn+j]]
+      f_sum=f_sum + conditional(((x-x_center[0])**2+(y-x_center[1])**2)**0.5>s,0,20*(1-((x-x_center[0])**2+(y-x_center[1])**2)/pow(s,2))**3*(3*((x-x_center[0])**2+(y-x_center[1])**2)+1))
+aexpr=1./(1+f_sum)
+aelt='CG'
+adeg=5
+A = assemble(interpolate(aexpr, FunctionSpace(mesh, aelt, adeg)))
+# evaluate coefficient, save to file and plot
+plt.clf()
+fig, axes = plt.subplots()
+collection = tripcolor(A, axes=axes)
+fig.colorbar(collection);
+plt.title('coefficient')
+plt.savefig("coef.png", dpi=500)
+print("> coefficient plotted to coef.png")
+
