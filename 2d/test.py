8fe3626b0f25c963b2b49844e883a21e3c583720
Qile <yanqile1993@outlook.com>
2024-08-15 13:13:20 -0500

    update

--- 2d/test.py
+++ 2d/test.py
@@ -20,14 +20,16 @@ U2 = np.random.uniform(size = nn**2)
 r = 0.2
 dn1 = r * np.sqrt(U2) * np.cos(2 * np.pi * U1)
 dn2 = r * np.sqrt(U2) * np.sin(2 * np.pi * U1)
-A=Function(FunctionSpace(mesh, 'CG', 7))
+Asum=Function(FunctionSpace(mesh, 'CG', 7))
 for i in range(0,nn):
    for j in range(0,nn):
       x_center=[i+1+dn1[i*nn+j],j+1+dn2[i*nn+j]]
       fi= conditional(((x-x_center[0])**2+(y-x_center[1])**2)**0.5>s,0,20*(1-((x-x_center[0])**2+(y-x_center[1])**2)/pow(s,2))**3*(3*((x-x_center[0])**2+(y-x_center[1])**2)+1))
-      Fi=assemble(interpolate(1.0/(1+fi), FunctionSpace(mesh, 'CG', 7)))
-      A+=Fi
-A+=-(nn**2-1)
+     # Fi=assemble(interpolate(1.0/(1+fi), FunctionSpace(mesh, 'CG', 7)))
+     # A+=Fi
+      Fi=assemble(interpolate(fi, FunctionSpace(mesh, 'CG', 7)))
+      Asum+=Fi
+A=assemble(interpolate(Constant(1.0)/(Constant(1.0)+Asum), FunctionSpace(mesh, 'CG', 7)))
 #print('defining coefficients')
 #A=assemble(interpolate(1.0/(1.0+Fs), FunctionSpace(mesh, 'CG', 3)))
 # evaluate coefficient, save to file and plot
