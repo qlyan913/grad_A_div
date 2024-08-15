00a7ac2eaa7cefea2c50e5f48f9b58719cf3295a
Qile <yanqile1993@outlook.com>
2024-08-15 09:00:30 -0500

    update

--- 2d/test.py
+++ 2d/test.py
@@ -7,7 +7,7 @@ from slepc4py import SLEPc
 import numpy as np
 from solver import *
 deg = 5
-L=40 # length of square
+L=20 # length of square
 nx=100
 ny=100
 mesh = SquareMesh(nx,ny,L)
@@ -20,12 +20,12 @@ U2 = np.random.uniform(size = nn**2)
 r = 0.2
 dn1 = r * np.sqrt(U2) * np.cos(2 * np.pi * U1)
 dn2 = r * np.sqrt(U2) * np.sin(2 * np.pi * U1)
-A=Function(FunctionSpace(mesh, 'CG', 3))
+A=Function(FunctionSpace(mesh, 'CG', 7))
 for i in range(0,nn):
    for j in range(0,nn):
       x_center=[i+1+dn1[i*nn+j],j+1+dn2[i*nn+j]]
       fi= conditional(((x-x_center[0])**2+(y-x_center[1])**2)**0.5>s,0,20*(1-((x-x_center[0])**2+(y-x_center[1])**2)/pow(s,2))**3*(3*((x-x_center[0])**2+(y-x_center[1])**2)+1))
-      Fi=assemble(interpolate(1.0/(1.0+fi), FunctionSpace(mesh, 'CG', 3)))
+      Fi=assemble(interpolate(1.0/(1.0+fi), FunctionSpace(mesh, 'CG', 7)))
       A+=Fi
 #print('defining coefficients')
 #A=assemble(interpolate(1.0/(1.0+Fs), FunctionSpace(mesh, 'CG', 3)))
