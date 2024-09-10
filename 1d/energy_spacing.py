d0632a844ad2e6c2f256dd0ad9cbdb23ffc69b65
qlyan913 <yanqile1993@outlook.com>
2024-09-10 13:36:21 -0500

    update

--- 1d/energy_spacing.py
+++ 1d/energy_spacing.py
@@ -1,12 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
-id=19
+id=15
 filename="Results/{:06d}/eigenvalues.txt".format(id)
 plot_file="Results/{:06d}/es_hist.png".format(id)
+plot_file2="Results/{:06d}/es_r_hist.png".format(id)
 eigens=np.loadtxt(filename)
 es = [eigens[i+1]-eigens[i] for i in range(len(eigens)-1)]
 es = np.array(es)
 
+r = [min(es[i],es[i+1])/max(es[i],es[i+1]) for i in range(len(es)-1)]
 average=sum(es)/len(es)
 es=es/average  # normalize
 x=np.linspace(0,8,1000)
@@ -18,3 +20,14 @@ plt.xlabel('s')
 plt.title('energy spacing with average energy space {}'.format(average))
 plt.savefig(plot_file)
 print("> histogram plotted to {}".format(plot_file))
+
+average2=sum(r)/len(r)
+plt.clf()
+plt.hist(r,bins=60,weights=np.ones(len(r))/len(r))
+x=np.linspace(0,1,100)
+y=2/(1+x)**2
+plt.plot(x,y,"r-")
+plt.xlabel('s')
+plt.title('energy spacing with average energy space ratio {}'.format(average2))
+plt.savefig(plot_file2)
+print("> histogram plotted to {}".format(plot_file2))
